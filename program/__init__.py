3033c003e7415985727fa95f65b72eec8154f8f3
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-07-25 10:46:19 -0500

    Update __init__.py

--- program/__init__.py
+++ program/__init__.py
@@ -1,1 +1,11 @@
 import file
+
+CREATOR = "ARACADERISE/GetStuffDone"
+VERSION = "1.0.1"
+START_DATE = "Jul 24th, 11pm, 2019"
+RELEASE_DATE = ""
+TYPE = ".ss file"
+
+def _return_stats():
+  return CREATOR, VERSION, START_DATE, RELEASE_DATA, TYPE
+_return_stats()
