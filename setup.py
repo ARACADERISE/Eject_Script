5ed9529951f9f1c704190fbfece2053bc1e98fbe
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-07-25 10:50:41 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup
 
 
-setup(name="slang",
-      version="1.0.1",
-      packages=["program"])
+setup(
+      name="slang",
+      version="1.0",
+      packages=["sLang"]
+)
