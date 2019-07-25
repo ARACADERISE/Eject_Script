3c845a38786bbbb95de84db1a3e849390ef00177
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-07-24 20:47:15 -0500

    Create __inti__.py

---
+++ __inti__.py
@@ -0,0 +1,11 @@
+# This file just has some basic info
+
+"""
+  * This project, I am hoping, is going to be a lazy form of writing a .sh file
+  * It is going to have, I hope, both strongly typed and weakly typed variables, functions and classes
+  * I, as of right now, just starting the project, do not know if it is going to work out
+  * I am going to try my best and complete this
+"""
+
+CREATOR = "ARACADERISE/GetStuffDone"
+VERSION = "1.0.1"
