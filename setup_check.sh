89f2eeb6beb3bda7decb47a23b4bd14a573f17ed
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-07-24 23:34:48 -0500

    Update setup_check.sh

--- setup_check.sh
+++ setup_check.sh
@@ -1,5 +1,4 @@
 if [ ! -d /data/data/com.termux/files/usr/bin/check ]; then
-  sh ./check
   mv -v check /data/data/com.termux/files/usr/bin
   chmod +x /data/data/com.termux/files/usr/bin/check
   echo "\n==> Type 'check -file' to check the .ss file. As of right now you are booting into the Python mode\n"
