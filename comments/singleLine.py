dfd0683c1352e158fec84bbbcf046bb92897a942
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-07-24 23:43:33 -0500

    Update singleLine.py

--- comments/singleLine.py
+++ comments/singleLine.py
@@ -1,5 +1,18 @@
 # SINGLE LINE COMMENTS
 
 """
-
+  * A single line comment #
+  * This will also format the color of the comment(white)
 """
+
+def comment(directory,file_name):
+  cmd = f"""
+  cd
+  cd {directory}
+  """
+  os.system(cmd)
+  op = open(file_name,'r')
+  if '#' in op.read():
+    print('Yes')
+  else:
+    print('No')
