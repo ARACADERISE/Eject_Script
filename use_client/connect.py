069153d335fd0718e25f5927bd1d8c2883c842cd
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-07-25 17:54:56 -0500

    Update connect.py

--- use_client/connect.py
+++ use_client/connect.py
@@ -3,4 +3,8 @@
 import json, os
 
 TYPE = 'Connection With Json'
-GET_CONNECTION = open(
+
+if os.path.exists('/data/data/com.termux/files/home/sLang/client.json'):
+  GET_CONNECTION = open('client.json','r')
+if not os.path.exists('/data/data/com.termux/files/home/sLang/client.json'):
+  raise Exception('Cannot connect to client.json')
