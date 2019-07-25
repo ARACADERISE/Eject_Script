94a0dd24303fc29c342f84b3835aaff350ad47af
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-07-24 23:38:35 -0500

    Update get.py

--- get.py
+++ get.py
@@ -5,23 +5,25 @@
   * Then will ask for the .ss filename
 """
 
-import os
+import os, time
+from colorama import Fore, Style
 
 os.system('bash setup_check.sh')
 
 def getFile():
-  directory = input("Director(if it's a folder within the directory then type directoryName/folderName) >> ")
-  file_name = input(".ss FileName >> ")
+  directory = input(Fore.MAGENTA+Style.BRIGHT+"Director(if it's a folder within the directory then type directoryName/folderName) >> "+Fore.WHITE)
+  file_name = input(Fore.MAGENTA".ss FileName >> "+Fore.WHITE)
   cmd = f"""
   cd
   cd {directory}
-  clear
   cat {file_name}
   """
   if '.ss' in file_name:
     # We want a line spacing
     print('\n')
     os.system('echo " ~~~FILE INFORMATION~~~"')
+    time.sleep(1)
+    os.system('clear')
     os.system(cmd)
     print('\n')
     return "Checking status started",1078
