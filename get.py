f619835591f9c13812531ebb062bcfee036da5f9
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-07-24 23:49:16 -0500

    Update get.py

--- get.py
+++ get.py
@@ -13,20 +13,20 @@ os.system('bash setup_check.sh')
 
 def getFile():
   directory = input(Fore.MAGENTA+Style.BRIGHT+"Director(if it's a folder within the directory then type directoryName/folderName) >> "+Fore.WHITE)
-  file_name = input(Fore.MAGENTA+".ss FileName >> "+Fore.WHITE)
+  file_name = input(Fore.MAGENTA+".ss FileName >> ")
   cmd = f"""
   cd
   cd {directory}
   cat {file_name}
   """
   if '.ss' in file_name:
-    comment(directory,file_name)
     # We want a line spacing
     print('\n')
     os.system('echo " ~~~FILE INFORMATION~~~"')
     time.sleep(1)
     os.system('clear')
     os.system(cmd)
+    comment(directory,file_name)
     print('\n')
     return "Checking status started",1078
 getFile()
