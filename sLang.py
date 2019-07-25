e0cd1a3bec5f3e0a6f28e26044dc603ea69c68e3
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-07-24 23:14:45 -0500

    Update sLang.py

--- sLang.py
+++ sLang.py
@@ -12,13 +12,7 @@ from colorama import Fore, Style
 os.system('bash setup_check.sh')
 
 def reader():
- directory = input(Fore.MAGENTA+'Directry >> '+Fore.WHITE)
- os.system(f'cd && cd {directory}')
- folder = input(Fore.MAGENTA+'.s File Name(with .ss) >> ')
- os.system(f'cat {folder}')
- if '.ss' in folder:
+ TYPE='.ss'
+ if TYPE == '.ss':
    os.system('check -file')
- else:
-   # If it isn't a .s file we will raise a exception
-   raise Exception(f"The file you wanted to open isn't a .s file. {Fore.YELLOW}Aborting..")
 reader()
