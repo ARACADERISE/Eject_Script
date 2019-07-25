0f1ff4234d18de78a482d6638434c170052e3298
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-07-24 23:20:18 -0500

    Update and rename _get_ss_file.py to get.py

--- _get_ss_file.py
+++
@@ -1,6 +0,0 @@
-# This Python script file will get the file, then lead into the reading process
-
-"""
-  * Will ask user to open a directory
-  * Then will ask for the .ss filename
-"""
---
+++ get.py
@@ -0,0 +1,17 @@
+# This Python script file will get the file, then lead into the reading process
+
+"""
+  * Will ask user to open a directory
+  * Then will ask for the .ss filename
+"""
+
+def getFile():
+  directory = input("Director(if it's a folder within the directory then type directoryName/folderName) >> ")
+  file_name = input(".ss FileName >> ")
+  cmd = f"""
+  cd
+  cd {directory}
+  cat {file_name}
+  """
+  if '.ss' in file_name:
+    os.system(cmd)
