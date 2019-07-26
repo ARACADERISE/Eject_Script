b75beb5938c6616bd32a89b11b103ec98fe869fa
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-07-26 12:46:49 -0500

    Update create_client_fore_platform_name.py

--- client_server/create_client_fore_platform_name.py
+++ client_server/create_client_fore_platform_name.py
@@ -73,4 +73,13 @@ try:
   bootup_timer = CLIENT_BOOTUP
   _set_user_client(CREATE_CLIENT,bootup_timer)
 finally:
-  pass
+  # For the fact that the .json file stores very simple data that doesn't change
+  # we will make it to where it uploades once
+  if not os.path.exists('/data/data/com.termux/files/home/sLang/client.json'):
+    def json_data(dumping):
+      with open('client.json','w') as c_j:
+        json.dump(dumping,c_j,indent=2,sort_keys=True)
+      return "Status upgrade with info in json files with exit status",1078
+    json_data(CLIENT)
+  if os.path.exists('/data/data/com.termux/files/home/sLang/client.json'):
+    pass
