7377748dbc0238c3b63fea7c7fe8481a8af629b2
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-07-31 18:42:55 -0500

    Update original_client_connection.py

--- use_client/original_client_connection.py
+++ use_client/original_client_connection.py
@@ -1,4 +1,12 @@
 # Connection to any json files throughout the project
+"""
+  * QUICK DEVELOPERS STATEMENT
+  * NOTE: I intentionally re-wrote each if statement for each signal
+  * due to the fact that writing a function that took in arguments would be more strictly typed
+  * due to the fact that if I were to make a function(def) it would have to take certain arguments that
+  * made the one if statement(which would be the use of a function, to only write one if statement) to work.
+  * I rather re-type the if statements for each signal and know it's gonna work than do a function and run into errors
+"""
 
 import json, os, sys
 
