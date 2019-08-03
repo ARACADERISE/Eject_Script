861aae2a311fa488895220e3e3dc676b6840f42a
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-08-03 15:13:02 -0500

    Delete setup_check.sh

--- setup_check.sh
+++
@@ -1,9 +0,0 @@
-if [ ! -d /data/data/com.termux/files/usr/bin/check ]; then
-  mv -v check /data/data/com.termux/files/usr/bin
-  chmod +x /data/data/com.termux/files/usr/bin/check
-  echo "\n==> Type 'check -file' to check the .ss file. As of right now you are booting into the Python mode\n"
-fi
-
-if [ -d /data/data/com.termux/files/usr/bin/check ]; then
-  echo "\n==> You already have 'check' rooted to your /bin directry. Type 'check -file', if you haven't booted yourself into the Pythons scripts, to check your .ss file"
-fi
