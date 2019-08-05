2fdf437eb020174f941a1709ae7df3df1a09de8f
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-08-04 23:49:41 -0500

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,1 +1,20 @@
+CREATE TABLE database (
+  ID INTEGER PRIMARY KEY,
+  USERSNAME TEXT NOT NULL UNIQUE,
+  LINKED_ACCOUNTS TEXT NOT NULL UNIQUE
+);
 
+INSERT INTO database (ID,USERSNAME,LINKED_ACCOUNTS)
+VALUES (1,'ARACADERISE','NONE');
+
+-- ADDING PRIMARY USER WEBPAGE LINK
+ALTER TABLE database
+ADD COLUMN webpage TEXT DEFAULT 'None';
+
+-- We want to update a value
+UPDATE database
+set webpage = 'https://'
+where wepage = 'None';
+
+SELECT DISTINCT *
+FROM database;
