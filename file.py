7efe638d55692cd10f36eaaf21db1af780317bd3
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-08-07 10:00:52 -0500

    Create file.py

---
+++ file.py
@@ -0,0 +1,16 @@
+"""
+  "Putting .txt, .db and .sql files to use"
+"""
+
+from start_engine import starter
+from begin_eject import ejector
+
+# Establish connection
+start = starter('connection.txt','18080','sql')
+start.getConnection(start_connection=True)
+
+# Start cache process
+start.startCaching()
+
+# Ejection process
+start.startEjecting();
