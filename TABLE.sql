96d6647bc093809011ae26a4e09162dd3150020c
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-08-13 13:14:32 -0500

    Update TABLE.sql

--- TABLE.sql
+++ TABLE.sql
@@ -1,8 +1,9 @@
 CREATE TABLE DATABASE_ (
   -- We want a id
   ID INTEGER PRIMARY KEY,
-  -- We want a name
-  NAME_ TEXT UNIQUE,
-  -- We want a token
-  TOKEN TEXT UNIQUE
+  -- We want a terminal type
+  SYSTEM_ TEXT NOT NULL,
+  --  WE WANT A TOKEN
+  -- For the fact that there can be more than one system in the database we will make it NOT NULL
+  TOKE TEXT NOT NULL
 );
