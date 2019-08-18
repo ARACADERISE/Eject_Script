5ebfc3605a9784e4b229e51412dd3045edf556e1
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-08-17 21:40:17 -0500

    Update setup_eject.sh

--- setup_eject.sh
+++ setup_eject.sh
@@ -1,1 +1,11 @@
+if [ ! -d /data/data/com.termux/files/usr/bin/Eject_Script ]; then
+  mv -v Eject_Script /data/data/com.termux/files/usr/bin
+  chmod +x /data/data/com.termux/files/usr/bin/Eject_Script
+  echo "==> Type: Eject_Script to run the Application!"
+  cd
+  exit
+fi
 
+if [ -d /data/data/com.termux/files/usr/bin/Eject_Script ]; then
+  echo "Looks you have already rooted 'Eject_Script' to /usr/bin\nType 'Eject_Script' to run the application"
+fi
