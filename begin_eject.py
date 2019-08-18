0afaff13e1718d51ad9c1e037f06a909d153e2f6
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-08-17 21:27:51 -0500

    Update begin_eject.py

--- begin_eject.py
+++ begin_eject.py
@@ -227,7 +227,7 @@ VALUES ({ID},'{TERMINAL_TYPE}','{TOKE_}')
           with open('CACHE.txt','w') as file:
             # we have to re-assign the values before for it's a .txt file
 
-            file.write("###CACHE_CONNECTION_DATA###\n\n#- This File Is The \"Catch File\" For All Data Being Transfered -#\n\n"+f" [#- Cache_Port_Opened_@_PORT:{self.port} -#]"+"\n\n"+f' [#- EJECTING_{self.upd_file_with} -#]'+'\n\n'+'  \n'+f'[#- EJECT_TABLE\n{open("TABLE.sql","r").read()} -#]'+'\n\n'+'\n\n'+'  \n[#- EJECT_SUCCESSFUL -#]")
+            file.write("###CACHE_CONNECTION_DATA###\n\n#- This File Is The \"Catch File\" For All Data Being Transfered -#\n\n"+f" [#- Cache_Port_Opened_@_PORT:{self.port} -#]"+"\n\n"+f' [#- EJECTING_{self.upd_file_with} -#]'+'\n\n'+'  \n'+f'[#- EJECT_TABLE\n{open("TABLE.sql","r").read()} -#]'+'\n\n'+'\n\n'+'  \n[#- EJECT_SUCCESSFUL -#]')
             file.close()
 
           return "Done with no errors and exit status {}".format(1078)
